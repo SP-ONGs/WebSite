4079743756eedc68170715438f7c623e798b0ab4
Ryan Gualberto <ryan13secondary@gmail.com>
2026-09-19 11:14:04 -0300

    [Hot-fix] Removed useless functions.

--- src/controllers/layoutController.js
+++ src/controllers/layoutController.js
@@ -1,21 +1,6 @@
 // --> Variables <-- //
 
 // React
-import { createContext, useContext, useState } from "react";
-const LayoutContext = createContext(null);
 
 
 // --> Functions <-- //
-export function LayoutProvider({ children }) {
-  const [modal, setModal] = useState("loginONG");
-
-  return (
-    <LayoutContext.Provider value={{ modal, setModal }}>
-      {children}
-    </LayoutContext.Provider>
-  );
-}
-
-export function useLayout() {
-  return useContext(LayoutContext);
-}
